f8ede674ca757697df557f0a85b10a0f9f5aeaa3
Anwesh43 <anweshthecool0@gmail.com>
2019-05-11 14:44:10 +0530

    adding view in main activity

--- app/src/main/java/com/anwesh/uiprojects/linkededgejointview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkededgejointview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkededgejointview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.edgejointview.EdgeJointView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EdgeJointView.create(this)
     }
 }
